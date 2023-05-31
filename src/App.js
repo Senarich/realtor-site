64e437cac81edd0aaec57be4b9ae7a64376f4788
Senarich <116800406+Senarich@users.noreply.github.com>
2023-06-01 00:07:09 +0100

    creates all the pages needed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import React from "react";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offer from "./pages/Offers";
+
+export default function App() {
   return (
     <>
-      <h1>hello world</h1>;
+      <Router>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offer />} />
+        </Routes>
+      </Router>
     </>
   );
 }
-
-export default App;
