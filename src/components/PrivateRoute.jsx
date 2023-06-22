8a5506bed80aeeb3ccf05f14c5322c01ef051c92
Senarich <116800406+Senarich@users.noreply.github.com>
2023-06-22 01:29:19 +0100

    created a private route and a custom hook for the protection of the profile page and the log-out functionality

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -5,7 +5,7 @@ import UseAuthStatus from "../hooks/UseAuthStatus";
 export default function PrivateRoute() {
   const { loggedIn, checkingStatus } = UseAuthStatus();
   if (checkingStatus) {
-    return <h3 className="text-center">Loading...</h3>
+    return <h3 className="text-center mt-8">Loading...</h3>;
   }
   return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 }
